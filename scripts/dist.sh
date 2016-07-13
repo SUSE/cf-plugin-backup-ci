b08532bb730c97b306a5cc620df9ee4919011848
Florin Dragos <florin.dragos@hpe.com>
2016-07-13 14:00:51 +0300

    fix tasks

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,5 +2,6 @@
 set -e
 PATH=$PATH:$PWD/bin
 GOPATH=$PWD
-make -C src/github.com/hpcloud/cf-plugin-backup windows_dist
+make -C src/github.com/hpcloud/cf-plugin-backup build
+make -C src/github.com/hpcloud/cf-plugin-backup dist
 cp src/github.com/hpcloud/cf-plugin-backup/*.tgz cf-plugin-backup/
