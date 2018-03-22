26ec7b3de6826d8677420fe18dc7d72b1f188c88
Andreas Kupries <andreas.kupries@suse.com>
2018-03-22 15:12:34 -0700

    Address comments.

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,13 +4,22 @@ PATH=$PATH:$PWD/bin
 GOPATH=$PWD
 
 SRC=${PWD}/src/github.com/SUSE/cf-plugin-backup
-TAG=$(git -C ${SRC} describe --tags --long | awk -F - '{ print $1 }' )
+TAG=$(git -C ${SRC} describe --tags --abbrev=0)
+LAST_TAG=$(git -C ${SRC} describe --tags --abbrev=0 HEAD^)
 NAME=$(basename $(git -C ${SRC} config --get remote.origin.url) .git)
+
+# Show incoming state
+echo NAME: ${NAME}
+echo TAG_: ${TAG}
+echo LAST: ${LAST_TAG}
 
 # Assemble configuration data for `out` to git-release resource.
 echo > release/name ${TAG}
 echo > release/tag  ${TAG}
 echo > release/body "${NAME}: Release ${TAG}"
+echo >> release/body ""
+git -C ${SRC} log --reverse --pretty=%s --merges ${LAST_TAG}...${TAG} \
+    >> release/body
 
 # Build the tarballs for the release
 make -C ${SRC} build dist
@@ -23,47 +32,30 @@ do
     cat $path
 done
 
- #Unpack the tarballs into the release.
+# Unpack the tarballs into the release.
 mkdir release/assets
 for path in ${SRC}/*.tgz
 do
     echo Processing ${path} ...
 
-    rm -rf tmp
-    mkdir  tmp
-    ( cd tmp ; tar xfz $path )
-
-    # tmp/cf-plugin-backup/README.txt
-    # tmp/cf-plugin-backup/License.md
-    # tmp/cf-plugin-backup/cf-plugin-backup.*
-
-    mv tmp/cf-plugin-backup tmp/ar
-    # tmp/ar/README.txt
-    # tmp/ar/LICENSE.md
-    # tmp/ar/cf-plugin-backup*
-
-    mv    tmp/ar/cf-plugin-backup* tmp/
-    mv    tmp/ar/*     release/assets/
-    rmdir tmp/ar
-    # tmp/cf-plugin-backup*
-
-    vbase=$(basename ${path} .tgz)
-    new=$(cd tmp ; ls cf-plugin-backup* | sed -e "s|cf-plugin-backup|${vbase}|")
-    # Replace the generic part of the name with a name containing
-    # version and platform information. Any platform specific
-    # extension of the filename (example: .exe) is preserved.
-
-    # Place binary into release
-    mv tmp/cf-* release/assets/${new}
-
-    # And calculate the adjunct checksum
-    ( cd release/assets ; sha256sum ${new} ) > release/assets/$(basename ${new} .exe).SHA256
-
-    rm -rf tmp
+    tar xfz "${path}" \
+	-C release/assets/ \
+	--strip-components=1 \
+	--overwrite \
+	--transform="s@/cf-plugin-backup@/$(basename ${path%%.tgz})@"
 done
 
+# And calculate the checksums for all the executables
+(
+    cd release/assets
+    for path in cf-*
+    do
+	sha256sum ${path} > $(basename ${path} .exe).SHA256
+    done
+)
+
 # Collect the release into a single tarball for S3 side load.
-tar -C release -czf - . > cf-plugin-backup/cf-plugin-backup-${TAG}-release.tgz
+tar czf cf-plugin-backup/cf-plugin-backup-${TAG}-release.tgz -C release .
 
 # Show assembly in log
 echo Assembled:
